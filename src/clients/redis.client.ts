a9f4801ad31d088cce73942b00ea4a5985b14842
Arvind Ramachandran <arvind.ramachandran@keeptruckin.com>
2021-04-25 01:10:51 +0530

    Remove TLS from Redis server

--- src/clients/redis.client.ts
+++ src/clients/redis.client.ts
@@ -1,4 +1,5 @@
 import Redis from 'ioredis';
+import { readFileSync } from 'fs';
 
 const REDISHOST = process.env.REDIS_IP || 'host.docker.internal';
 const REDISPORT = process.env.REDIS_PORT || 6379;
@@ -7,4 +8,6 @@ export const client = new Redis(Number(REDISPORT), REDISHOST, {
   enableAutoPipelining: true,
   lazyConnect: true,
 });
-client.on('error', (err) => console.error('ERR:REDIS:', err));
+client.on('error', (err) => {
+  console.error('ERR:REDIS:', err);
+});
